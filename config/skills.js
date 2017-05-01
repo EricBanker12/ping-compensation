3e3c7491920fb6c6195968f4d5c725c7b7e227d0
Pinkie Pie <yarnovax@gmail.com>
2017-05-01 07:16:18 -0400

    Fix a couple invalid Mystic chains

--- config/skills.js
+++ config/skills.js
@@ -1237,6 +1237,7 @@ module.exports = {
 		8: { // Metmorphic Blast
 			0: {
 				length: 820,
+				noInterrupt: [1],
 				chains: {
 					8: 30,
 					23: 30
@@ -1299,6 +1300,7 @@ module.exports = {
 		23: { // Metmorphic Smite
 			0: {
 				length: 1430,
+				noInterrupt: [1],
 				chains: {
 					8: 30
 				}
