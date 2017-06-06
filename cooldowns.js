945098297f4fb6b269c046c891b9c61c52b0857f
Pinkie Pie <yarnovax@gmail.com>
2017-06-05 21:57:46 -0400

    Ignore item cooldowns
    Item cooldowns are in seconds, so can't be fixed in this way.

--- cooldowns.js
+++ cooldowns.js
@@ -3,13 +3,10 @@ const Ping = require('./ping')
 module.exports = function Cooldowns(dispatch) {
 	const ping = Ping(dispatch)
 
-	dispatch.hook('S_START_COOLTIME_SKILL', 1, cooldown)
-	dispatch.hook('S_START_COOLTIME_ITEM', 1, cooldown)
-
-	function cooldown(event) {
+	dispatch.hook('S_START_COOLTIME_SKILL', 1, event => {
 		if(event.cooldown > 0) {
 			event.cooldown -= ping.min
 			return event.cooldown > 0
 		}
-	}
+	})
 }
