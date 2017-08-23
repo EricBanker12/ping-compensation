7f35d39685ef9f75db925b1177bb41d272e0fa11
Pinkie Pie <yarnovax@gmail.com>
2017-08-23 13:47:40 -0400

    Fix crash on invalid end with debugging enabled

--- skills.js
+++ skills.js
@@ -698,7 +698,7 @@ module.exports = function SkillPrediction(dispatch) {
 				if(DEBUG_LOC) strs.push(...[event.w + '\xb0', '(' + Math.round(event.x), Math.round(event.y), Math.round(event.z) + ')'])
 
 				strs.push(...[
-					(Math.round(Math.sqrt(Math.pow(event.x - serverAction.x, 2) + Math.pow(event.y - serverAction.y, 2)) * 1000) / 1000) + 'u',
+					(serverAction ? (Math.round(Math.sqrt(Math.pow(event.x - serverAction.x, 2) + Math.pow(event.y - serverAction.y, 2)) * 1000) / 1000) : '???') + 'u',
 					duration + 'ms',
 					'(' + Math.round(duration * serverAction.speed) + 'ms)'
 				])
