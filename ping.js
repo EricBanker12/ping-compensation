f6bf11cd00e86611a6477ce4a3ab0f5fccadaac2
undefined3394 <32277378+undefined3394@users.noreply.github.com>
2017-12-17 02:33:52 +0100

    Update ping.js

--- ping.js
+++ ping.js
@@ -52,8 +52,8 @@ class Ping {
       return ((id > 0x4000000) ? id - 0x4000000 : id);
     };
     
-    //dispatch.hook('C_CHECK_VERSION', 1, pingStart.bind(null, 1));
-    //dispatch.hook('S_CHECK_VERSION', 1, pingEnd.bind(null, 1));
+    dispatch.hook('C_CHECK_VERSION', 1, pingStart.bind(null, 1));
+    dispatch.hook('S_CHECK_VERSION', 1, pingEnd.bind(null, 1));
     dispatch.hook('S_LOGIN', 1, e => { ({cid} = e); });
 
     const skillHook = e => {
