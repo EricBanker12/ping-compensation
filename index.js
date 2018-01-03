3344c74ff8742cdddddca4af445a884c7c940d36
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-01-03 16:19:28 +0300

    Reworked loader
    hookOnce for CCheckVersion, updated list with blocked modules, forced compatability check, blocking problems with copypasted sp folders

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-const CHECK_COMPATABILITY = true;
+//------------------------------------
+//by SaltyMonkey 
+//Reworked loader for skill prediction
+//In theory a bit slower with bunch of modules than old loader
+//but can detect copypasted SP instances
+//------------------------------------
 
-let error = false;
-
-if(CHECK_COMPATABILITY){
-	try {
-		require('command')
-		
-	}
-	catch(e){
-		error = true;
-		console.error('ERROR: Skill Prediction require "Commands" module for work')
-	}
-	for(let mod of ['cooldowns', 'lockons', 'lockons-master', 'fastfire', 'fast-fire', 'fast-fire-master', 'fast-block'])
-		try {
-			require(mod);
-			console.error('ERROR: Skill Prediction is not compatible with the obsolete mod "' + mod + '", please remove it and try again.');
-			console.error('**For advanced users only**: To disable compatability checking, edit index.js and set CHECK_COMPATABILITY to false');
-			error = true;
-			break
-		}
-		catch(e) {}
-}
-
+const { lstatSync, readdirSync } = require('fs');
+const path = require('path');
 const sysmsg = require('tera-data-parser').sysmsg,
-	MODS = [
+	childModules = [
 		require('./lib/core'),
 		require('./lib/cooldowns')
 	];
 
-module.exports = function SkillPredictionCore(dispatch) {
-	if(error) return;
+//filter isDirectory => true
+const isDirectory = source => lstatSync(source).isDirectory();
 
-	dispatch.hook('C_CHECK_VERSION', 1, () => {
-		if(!sysmsg.maps.get(dispatch.base.protocolVersion) || sysmsg.maps.get(dispatch.base.protocolVersion).name.size === 0) {
+//filter "module without _" => true
+const isActiveModule = source => !source.includes('_');
+
+//function return "c" from "a/b/c" 
+const getShortDirName = source => source.slice(source.lastIndexOf(path.sep) + 1, source.length);
+
+//function return short names for all active modules from folder with modules
+const getModules = source =>
+	(readdirSync(source).map(name => path.join(source, name))
+		.filter(isDirectory))
+		.map(elem => getShortDirName(elem))
+		.filter(isActiveModule);
+
+//------------------------------------------------------------------------
+let currentDir = getShortDirName(__dirname);
+let blockedModules = ['cooldowns', 'lockons', 'lockons-master', 'fastfire', 'fast-fire', 'fast-fire-master', 'fast-block',
+	'skill-prediction', 'skill-prediction-master', 'skill-prediction-exp', 'skill-prediction-experimental',
+	'sp', 'cooldowns-master', 'fast-block-master', 'skillprediction', 'pinkie-sp', 'sp-pinkie'];
+let errorState = false;
+let installedModules = null;
+//------------------------------------------------------------------------
+
+//all installed modules except current dir
+installedModules = (getModules(path.resolve(__dirname, '../'))).filter(element => element !== currentDir);
+
+//check for blocked modules
+if (installedModules.some(element => blockedModules.indexOf(element) >= 0)) {
+	console.log('[Skill Prediction] ERROR! Blocked modules installed. Close tera-proxy and delete them.');
+	errorState = true
+}
+//check for "command"
+if (!installedModules.includes('command') && !installedModules.includes('command-master')) {
+	console.log('[Skill Prediction] ERROR! Missing module \'Command\'. Close tera-proxy and install it.');
+	errorState = true
+}
+
+module.exports = function SkillPredictionCore(dispatch) {
+	if (errorState) return;
+
+	dispatch.hookOnce('C_CHECK_VERSION', 1, () => {
+		if (!sysmsg.maps.get(dispatch.base.protocolVersion) || sysmsg.maps.get(dispatch.base.protocolVersion).name.size === 0) {
 			console.error('ERROR: Your version of tera-proxy is too old to run Skill Prediction');
 			process.exit()
 		}
 	});
 
-	for(let mod of MODS) mod(dispatch)
+	for (let mod of childModules) mod(dispatch)
 };
