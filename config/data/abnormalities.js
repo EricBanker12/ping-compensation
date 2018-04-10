d5a05a617dab02ddbc21506074ecec873e88603d
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-04-10 20:17:12 +0300

    Version 4.7
    * index.js removed
    * disabled jaunt (while fix in test)
    * automatic abnormies generator
    * small part from my utils (first step at rewrite)
    * changed loader (can remove old files with migration)
    * in /config/ folder only preset and config.json
    * automatic config file restore at start (if missing)
    * just block start for sp without proxy crash
    * maybe smth another

---
+++ config/data/abnormalities.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	10152085: true, // Mana Missiles (unused)
+	10152086: true, // Arc Bomb (unused)
+	10153093: true, // Rocket Jump & Rolling Reload (Memory leak >//>[status]) 
+}
