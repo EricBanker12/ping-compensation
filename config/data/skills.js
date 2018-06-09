c626e181203bacf053a3b692148c04eb591ece2e
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-06-09 22:01:41 +0300

    miss you, God Leyki

--- config/data/skills.js
+++ config/data/skills.js
@@ -6764,10 +6764,11 @@ module.exports = {
 		},
 		10: { // Arc Bomb
 			'*': {
+				abnormals: { 10152011: { chain: 2}},
+				consumeAbnormal: [10152011],
 				length: 1320,
 				noInterrupt: [10, 20],
 				chains: {
-					1: null,
 					'2-1': null,
 					3: null,
 					4: null,
