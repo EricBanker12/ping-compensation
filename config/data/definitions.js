2c0a22729ffd2a6b08216a8824c92ca89a833f82
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2018-07-14 20:14:21 +0300

    Internal change: multi patch support (alpha version)

---
+++ config/data/definitions.js
@@ -0,0 +1,176 @@
+//------------------------------------
+//by SaltyMonkey 
+//List with definitions
+//based on patch
+//
+//Example
+// "Def name": { "default": <def version>, <major version>: < def version>}
+//
+//------------------------------------
+
+module.exports = {
+	"S_LOGIN": {
+		71: 10,
+		75: 11
+	},
+	"S_RETURN_TO_LOBBY": {
+		"default": 1
+	},
+	"S_CREATURE_LIFE": {
+		"default": 2
+	},
+	"S_ABNORMALITY_BEGIN": {
+		"default": 2,
+		75: 3
+	},
+	"S_ABNORMALITY_REFRESH": {
+		"default": 1,
+		75: 1
+	},
+	"S_ABNORMALITY_END": {
+		"default": 1
+	},
+	"S_START_COOLTIME_SKILL": {
+		"default": 2,
+		75: 3
+	},
+	"S_DEFEND_SUCCESS": {
+		"default": 2,
+		75: 3
+	},
+	"S_CANNOT_START_SKILL": {
+		"default": 1,
+		75: 2
+	},
+	"C_CAN_LOCKON_TARGET": {
+		"default": 2,
+		75: 3
+	},
+	"S_CAN_LOCKON_TARGET": {
+		"default": 2,
+		75: 3
+	},
+	"C_PLAYER_LOCATION": {
+		"default": 5
+	},
+	"C_NOTIFY_LOCATION_IN_ACTION": {
+		"default": 3,
+		75: 4
+	},
+	"C_NOTIFY_LOCATION_IN_DASH": {
+		"default": 3,
+		75: 4
+	},
+	"C_START_SKILL": {
+		"default": 6,
+		75: 7
+	},
+	"C_START_TARGETED_SKILL": {
+		"default": 5,
+		75: 6
+	},
+	"C_START_COMBO_INSTANT_SKILL": {
+		"default": 3,
+		75: 4
+	},
+	"C_START_INSTANCE_SKILL": {
+		"default": 4,
+		75: 5
+	},
+	"C_START_INSTANCE_SKILL_EX": {
+		"default": 4,
+		75: 5
+	},
+	"C_PRESS_SKILL": {
+		"default": 3,
+		75: 4
+	},
+	"C_NOTIMELINE_SKILL": {
+		"default": 2,
+		75: 3
+	},
+	"S_GRANT_SKILL": {
+		"default": 2,
+		75: 3
+	},
+	"S_INSTANT_DASH": {
+		"default": 3
+	},
+	"S_INSTANT_MOVE": {
+		"default": 3
+	},
+	"S_ACTION_END": {
+		"default": 4,
+		75: 5
+	},
+	"C_CANCEL_SKILL": {
+		"default": 2,
+		75: 3
+	},
+	"S_ACTION_STAGE": {
+		"default": 6,
+		75: 7
+	},
+	"S_CREST_MESSAGE": {
+		"default": 2
+	},
+	"S_PLAYER_STAT_UPDATE": {
+		"default": 9
+	},
+	"S_CREST_INFO": {
+		"default": 2
+	},
+	"S_CREST_APPLY": {
+		"default": 2
+	},
+	"S_PLAYER_CHANGE_STAMINA": {
+		"default": 1
+	},
+	"S_SPAWN_ME": {
+		"default": 3
+	},
+	"S_USER_STATUS": {
+		"default": 1
+	},
+	"S_INVEN": {
+		"default": 14
+	},
+	"S_PARTY_MEMBER_LIST": {
+		"default": 7
+	},
+	"S_LEAVE_PARTY": {
+		"default": 1
+	},
+	"S_MOUNT_VEHICLE_EX": {
+		"default": 1
+	},
+	"S_UNMOUNT_VEHICLE_EX": {
+		"default": 1
+	},
+	"S_MOUNT_VEHICLE": {
+		"default": 2
+	},
+	"S_UNMOUNT_VEHICLE": {
+		"default": 2
+	},
+	"S_SYSTEM_MESSAGE": {
+		"default": 1
+	},
+	"C_REQUEST_GAMESTAT_PING": {
+		"default": 1
+	},
+	"S_RESPONSE_GAMESTAT_PONG": {
+		"default": 1
+	},
+	"S_EACH_SKILL_RESULT": {
+		"default": 10,
+		75: 12
+	},
+	"S_SKILL_LIST": {
+		"default": 1
+	},
+	"S_DECREASE_COOLTIME_SKILL": {
+		"default": 2,
+		75: 3
+	},
+};
