b520b012a0446f061e67b1f83eed1b39feab3c67
mister-kay <mister-kay@github.com>
2018-08-16 10:49:38 -0500

    update for patch 74

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = function PingCompensation(dispatch) {
-
+    /*
     if (dispatch.base.majorPatchVersion >= 74) {
         console.log('ping-compensation - KTera definitions unsupported')
         return
     }
-
+    */
+   
     const config = require('./config/config.json')
     const childModules = [require('./lib/core.js')]
     
