0370da8eec89e13a48bd07d4ecaf359132816647
Mister-Kay <private-information>
2018-10-05 06:55:19 -0500

    minor performance fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function PingCompensation(dispatch) {
     }
     */
    
-    const config = require('./config/config.json')
+    //const config = require('./config/config.json')
     const childModules = [require('./lib/core.js')]
     
     //if (!config.spCompatible) childModules.push(require('./lib/cooldowns.js'))
