c7b19faf21f6459702df9a9b6dae5259ade1e12f
Unknow <34173892+Unkn01w@users.noreply.github.com>
2018-12-04 04:18:03 +0100

    hi

--- config/skills.js
+++ config/skills.js
@@ -144,7 +144,7 @@ module.exports = {
 		29: { // Blade Draw
 			'*': { hasChains: true },
 			0: {
-				noInterrupt: [1, 2, 3, 4, 8, 9, 10, 11, 12, 13, '16-0', 18, '19-0', 21, 22, 27, 29, 34, 36, 37, 41, 42],
+				noInterrupt: [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, "16-0", 18, "19-0", 21, 22, 23, 27, 29, 30, 34, 35, 36, 37, 38, 41, 42],
 				interruptibleWithAbnormal: { 102010: 3 },
 				categoryChains: {
 					1032: 30,
